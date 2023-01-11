a0dfb16d2b21e4043287a2bf39b145419ce63f1b
unknown <circwallpapers@gmail.com>
2023-01-11 19:25:29 +0100

    'wednesday'

--- src/Nav.js
+++ src/Nav.js
@@ -12,13 +12,13 @@ const Nav = ({menu,setMenu,displayMenu}) =>{
         <nav className='nav-bar text-center'>
             <ul className={menu===true? 'navbar-nav' : 'collapsedMenu'}>
             {/*<Icon.ClockFill onClick={displayMenu} className={menu===false ? 'collapsed iconss': 'menubtn'}/>*/}
-                <li>
-                <div  className='headerlink' >
-                    <a href="#app">
-                     <img src={myLogo}/>
-                    </a>
-                    <h5 className="container circ mt-2">ircwall</h5>
-                </div>
+                <li className="geep">
+                    <div  className='headerlink' >
+                        <a href="#app">
+                        <img src={myLogo}/>
+                        </a>
+                        <h5 className="container circ mt-2">ircwall</h5>
+                    </div>
                 </li>
                 <li><a href='#about' title="About"><Icon.PersonFill className="iconss"/><h3>About</h3></a></li>
                 <li><a href="#technologies" title="Technologies"><Icon.TencentQq className="iconss"/><h3>Technologies</h3></a></li>
